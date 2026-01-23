50f40a3edc24cde9f29a30ee2a821bf9b5736c76
R-World007 <saimoehein2023@gmail.com>
2026-01-22 23:51:27 -0500

    lesson1_2done

--- Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Contents.swift
@@ -13,10 +13,13 @@ print("How to use playgrounds to make writing Swift fun and simple")
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
 
-
+print("Song Title: Above the Neck")
+print("Artist: Sofia")
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print("Everything above the neck")
+print("Everything but the imitation of youth must be given up at the desk")
+print("Everything but the look that sells")
 
 //:page 14 of 16  |  [Next: Exercise: Go! Fight! Win!](@next)
